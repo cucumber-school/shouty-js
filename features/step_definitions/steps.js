7576122afdfda3193cacadd2ed418bab330c031c
Federico Iachetti <iachetti.federico@gmail.com>
2020-02-03 09:44:49 -0300

    Added steps.js

---
+++ features/step_definitions/steps.js
@@ -0,0 +1,16 @@
+const { Given, When, Then } = require('cucumber');
+
+Given('Lucy is located 15m from Sean', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('Sean shouts “free bagels at Sean’s”', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('Lucy hears Sean’s message', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
